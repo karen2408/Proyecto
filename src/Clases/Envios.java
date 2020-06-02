8255a5999e6bb483191d2e36bde9157932869fc0
NOEMI RESENDIZ <66224820+NWolf974@users.noreply.github.com>
2020-06-01 20:06:56 -0500

    Update Envios.java

--- src/Clases/Envios.java
+++ src/Clases/Envios.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Clases;
+package Clases.Compra;
 
 /**
  *
  * @author Karen Cruz
  */
-public class Envios {
+public class Envios extends Compra{
     
 }
