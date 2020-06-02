773e5902d14ead7ea0e0a21a719b7619fee23e4a
NOEMI RESENDIZ <66224820+NWolf974@users.noreply.github.com>
2020-06-01 19:48:57 -0500

    Update: se agregaron los métos correspondientes

--- src/Clases/Compra.java
+++ src/Clases/Compra.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//hecho por Noemi Resendiz
+// este codigo no muestra la clase de comprar el cual hereda datos de la clase MostrarArticuloUsuario
 package Clases;
 
-/**
- *
- * @author Karen Cruz
- */
-public class Compra {
+public class Compra extends MostrarArticuloUsuario {
+    //este método nos muestra el total de pago de la compra
+private void TotalaPagar(){
+   }
+   //este método nos da a elegir nuestra forma de pago
+private void FormaDePago(){
+ }
+  //este método nos da a elegir nuestra forma de envio
+private void Envios(){
+      {
+    
     
 }
