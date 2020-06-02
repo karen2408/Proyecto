558fad0fdda6f66ebb71e5cc01fe9986c3beb7e0
NOEMI RESENDIZ <66224820+NWolf974@users.noreply.github.com>
2020-06-01 19:22:10 -0500

    Update and rename MostrarArtivuloUsuario.java to MostrarArticuloUsuario.java

---
+++ src/Clases/MostrarArticuloUsuario.java
@@ -0,0 +1,34 @@
+//hecho por Noemi Resendiz 
+//este codigo nos muestra a la clase mostrar articulo el cual herda dato de usuario
+package Clases;
+
+/**
+ *
+ * 
+ */
+public class MostrarArticuloUsuario extends Usuario{
+  //método privado  categoria  
+ private void categoria(){
+ }
+  //método privado  caracteristicas de el articulo
+  private void carateristicas(){
+ }
+  //método privado  Mostrar articulo 
+  private void MostrarArticulo(){
+ }
+    //método privado  Mostrar el precio
+private void Precio (){
+}
+ //método privado  Mostrar la existencia del articulo
+  private void Existencia(){
+}
+//método privado  Mostrar la existencia del articulo
+private void cantidad(){
+}
+  
+  
+  
+  
+  
+  
+}
